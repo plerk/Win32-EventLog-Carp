d4e43151d812346fb57be8798e3df3a73cc76ce6
Robert Rothenberg <rrwo@cpan.org>
2001-01-11 04:05:44 +0000

    version 1.00

--- Carp.pm
+++ Carp.pm
@@ -5,70 +5,123 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
 require Exporter;
 
-@ISA = qw( Exporter );
-@EXPORT = qw( confess carp croak );
+@ISA       = qw( Exporter Carp );
+@EXPORT    = qw( confess carp croak );
 @EXPORT_OK = qw( cluck click );
 
-$VERSION = '0.04';
+$VERSION   = '1.00';
 
-require Carp;
+use Carp;
 use Win32::EventLog;
 
-sub carp
-{
-    my $warning = Carp::shortmess(@_);
-    &_report( EVENTLOG_WARNING_TYPE, $warning );
-    warn $warning;
-}
-
-sub croak
-{
-    my $warning = Carp::shortmess(@_);
-    &_report( EVENTLOG_ERROR_TYPE, $warning );
-    die $warning;
-}
-
-sub confess
-{
-    my $warning = Carp::longmess(@_);
-    &_report( EVENTLOG_ERROR_TYPE, $warning );
-    die $warning;
-}
-
-sub cluck
-{
-    my $warning = Carp::longmess(@_);
-    &_report( EVENTLOG_WARNING_TYPE, $warning );
-    warn $warning;
-}
-
-sub click
-{
-    my $warning = join("", @_) . "\n"; # Carp joins elements w/no separator
-    &_report( EVENTLOG_INFORMATION_TYPE, $warning );
-    print STDERR $warning;
-}
+my ($log);
 
 sub _report
-{
+
+# Reports an event in the Windows NT event log.
+
+  {
+    # If for some reason the log isn't initialized...!
+
+    unless ($log)
+      {
+	CORE::die
+	  "Win32::EventLog not initialized in Win32::EventLog::Carp";
+	return;
+      }
+
     my ($event_type, $event_text) = @_;
 
-    $event_text =~ s/\n(?!\z)/\; /g; # change newlines to semicolons
-    $event_text =~ s/\n\z//;
+    # Change newlines to semicolons ('; ') since they do not show up well in
+    # the event log viewer (does Windows 2000 handle newlines better?)
 
-    my $eh = Win32::EventLog->new('Application');
+    $event_text =~ s/(\r?\n){1,}(?!\z)/\; /g;
+    $event_text =~ s/\r?\n\z//;
 
-    my %ReportHash = (
-	EventType => $event_type,
-	Source => $0,
-	Strings => $event_text
-    );
+    my $event = {
+		 EventType => $event_type,
+		 Source    => $0,
+		 Strings   => $event_text
+		};
 
-    $eh->Report( \%ReportHash );
+    $log->Report( $event );
+  }
 
-    $eh->Close();
-}
+sub _warn
+  {
+    _report( EVENTLOG_WARNING_TYPE, @_ );
+    CORE::warn @_;
+  }
 
+sub _die
+  {
+    _report( EVENTLOG_ERROR_TYPE, @_ );
+    CORE::die @_; 
+  }
+
+
+BEGIN
+  {
+
+    # Create a handle to the Windows NT event log (we do this in the BEGIN
+    # block so that we can trap some compilation errors).
+
+    $log    = Win32::EventLog->new('Application')
+      or die "Unable to initialize Windows NT event log";
+
+    # If there's already a handler, we just report the error to the log and
+    # go to the existing handler; otherwise we use our own.
+
+    if ($SIG{__WARN__})
+      {
+	my $previous = $SIG{__WARN__};
+	$SIG{__WARN__} = sub {
+	  _report( EVENTLOG_WARNING_TYPE, @_ );
+	  return &$previous;
+	};
+      }
+    else
+      {
+	$SIG{__WARN__} = \&Win32::EventLog::Carp::_warn;
+      }
+
+    if ($SIG{__DIE__})
+      {
+	my $previous = $SIG{__DIE__};
+	$SIG{__DIE__} = sub {
+	  _report( EVENTLOG_ERROR_TYPE, @_ );
+	  return &$previous;
+	};
+      }
+    else
+      {
+	$SIG{__DIE__} = \&Win32::EventLog::Carp::_die;
+      }
+
+  }
+
+END
+  {
+
+    # Make sure we clean up after ourselves!
+
+    if ($log)
+      {
+	$log->Close();
+      }
+  }
+
+sub carp    { CORE::warn Carp::shortmess @_;    }
+sub croak   { CORE::die  Carp::shortmess @_;   }
+sub cluck   { CORE::warn Carp::longmess @_;   }
+sub confess { CORE::die  Carp::longmess @_; }
+
+sub click
+  {
+    my $message = Carp::shortmess(@_);
+    &_report( EVENTLOG_INFORMATION_TYPE, $message );
+    print STDERR $message;
+  }
 
 1;
 __END__
@@ -77,51 +130,86 @@ __END__
 
 Win32::EventLog::Carp - for carping in the Windows NT Event Log
 
-=head1 SYNOPSIS
-
-  use Win32::EventLog::Carp;
-
-  my $code = &something() or croak "Unable to do something";
-
-  unless ($code)
-  {
-      carp "Nothing from something";
-  }
-
-=head1 DESCRIPTION
-
-Win32::EventLog::Carp is a wrapper for the Carp module so that messages are
-added to the Windows NT Event Log. This is useful for Perl scripts which run
-as services or through the scheduler.
-
-The interface is basically the same as Carp and it should function in the
-same way (though it has not been tested as thoroughly). You need only change
-references of "Carp" to "Win32::EventLog::Carp" to begin using this module.
-
-One notable exception is the addition of the I<click> function:
-
-  Win32::EventLog::Carp::click "Make a jazz noise here.";
-
-This outputs the message to STDERR without a stack trace, and allows scripts\
-to post a simple "I have started" or "I am doing XYZ now" message to the log.
-
-All messages are posted the the Application Log we well as to STDERR.
-
 =head1 REQUIREMENTS
 
   Carp
   Win32::EventLog
 
+=head1 SYNOPSIS
+
+  use Win32::EventLog::Carp;
+  croak "We're outta here!";
+
+  use Win32::EventLog::Carp qw(cluck);
+  cluck "This is how we got here!";
+
+=head1 DESCRIPTION
+
+C<Win32::EventLog::Carp> traps warnings and fatal errors in Perl and reports
+these errors in the Windows NT Event Log. This is useful for scripts which
+run as services or through the scheduler, and for CGI/ISAPI scripts.
+
+The interface is similar to C<Carp>: the C<carp>, C<croak> and C<confess>
+functions are exported (with C<cluck> being optional).  You need only change
+references of "Carp" to "Win32::EventLog::Carp" to begin using this module.
+
+One notable exception is the addition of the C<click> function:
+
+  Win32::EventLog::Carp::click "Hello!\n";
+
+This outouts a message to STDERR with a short stack trace and allows scripts
+to post a simple "I have started" or "I am doing XYZ now" message to the log.
+To avoid the stack trace, end the message with a newline (which is what
+happens with the C<Carp> module).
+
+All messages are posted the the Application Log we well as to STDERR.
+
+=head2 Using Win32::EventLog::Carp with CGI::Carp
+
+Some modules which trap the C<__WARN__> and C<__DIE__> signals are not very
+friendly, and will cancel out existing traps. The solution is to use this
+module I<after> using other modules:
+
+  use CGI::Carp;
+  use Win32::EventLog::Carp
+
+or
+
+  BEGIN
+    {
+      $SIG{__WARN__} = \&my_handler;
+    }
+
+  use Win32::EventLog::Carp
+
+It is assumed that the previous handler will properly C<warn> or C<die> as
+appropriate. This module will instead report these events to the NT event
+log.
+
+=head2 eval and die
+
+This module I<will> log errors in the event log when something dies in an
+eval.  This is a feature, not a bug.
+
 =head1 BUGS
 
-Any bugs in Win32::EventLog and Carp will lead to bugs with this module.
+Bugs in C<Win32::EventLog> and C<Carp> may lead to bugs with this
+module.
+
+=head1 SEE ALSO
+
+  Carp
+  CGI::Carp
+  Win32::EventLog
 
 =head1 AUTHOR
 
 Robert Rothenberg <rrwo@cpan.org>
 
-=head1 SEE ALSO
+=head1 LICENSE
 
-Win32::EventLog, Carp
+Copyright (c) 2000-2001 Robert Rothenberg. All rights reserved.
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
 
 =cut
